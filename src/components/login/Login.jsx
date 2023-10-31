b2a3d6ffb335e644e0aba483edde9aeaaad6b3e6
Jimmy Bonilla <jimmyb091202@gmail.com>
2023-10-31 00:35:21 -0700

    a

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -2,6 +2,21 @@ import React, { useState, useEffect, useContext } from 'react'
 import { useHistory, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../App';
 
+function getCookie(name) {
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+      const cookies = document.cookie.split(';');
+      for (let i = 0; i < cookies.length; i++) {
+          const cookie = cookies[i].trim();
+          if (cookie.startsWith(name + '=')) {
+              cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+              break;
+          }
+      }
+  }
+  return cookieValue;
+}
+
 const Login = () => {
     const navigate = useNavigate();
     const [session, setSession] = useContext(AuthContext);
